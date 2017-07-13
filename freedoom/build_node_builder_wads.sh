2f5bc60db43a08c31537b0934fb848f5564ccffc
Charlie Gunyon <charles.gunyon@gmail.com>
2017-07-13 00:02:29 -0400

    Parameterize the WAD node building script

--- freedoom/build_node_builder_wads.sh
+++ freedoom/build_node_builder_wads.sh
@@ -2,29 +2,31 @@
 
 set -e
 
-zdbsp freedoom2.wad -t             -o freedoom2-zdbsp-normal-uncompressed.wad
-zdbsp freedoom2.wad -t       -X    -o freedoom2-zdbsp-extended-uncompressed.wad
-zdbsp freedoom2.wad -t          -z -o freedoom2-zdbsp-normal-compressed.wad
-zdbsp freedoom2.wad -t       -X -z -o freedoom2-zdbsp-extended-compressed.wad
+BASE_WAD_NAME=$(basename $1 .wad)
 
-zdbsp freedoom2.wad -t -g          -o freedoom2-zdbsp-gl-normal-uncompressed.wad
-zdbsp freedoom2.wad -t -g    -X    -o freedoom2-zdbsp-gl-extended-uncompressed.wad
-zdbsp freedoom2.wad -t -g       -z -o freedoom2-zdbsp-gl-normal-compressed.wad
-zdbsp freedoom2.wad -t -g    -X -z -o freedoom2-zdbsp-gl-extended-compressed.wad
+zdbsp ${BASE_WAD_NAME}.wad -t             -o ${BASE_WAD_NAME}-zdbsp-normal-uncompressed.wad
+zdbsp ${BASE_WAD_NAME}.wad -t       -X    -o ${BASE_WAD_NAME}-zdbsp-extended-uncompressed.wad
+zdbsp ${BASE_WAD_NAME}.wad -t          -z -o ${BASE_WAD_NAME}-zdbsp-normal-compressed.wad
+zdbsp ${BASE_WAD_NAME}.wad -t       -X -z -o ${BASE_WAD_NAME}-zdbsp-extended-compressed.wad
 
-zdbsp freedoom2.wad -t -g -G       -o freedoom2-zdbsp-glmatching-normal-uncompressed.wad
-zdbsp freedoom2.wad -t -g -G -X    -o freedoom2-zdbsp-glmatching-extended-uncompressed.wad
-zdbsp freedoom2.wad -t -g -G    -z -o freedoom2-zdbsp-glmatching-normal-compressed.wad
-zdbsp freedoom2.wad -t -g -G -X -z -o freedoom2-zdbsp-glmatching-extended-compressed.wad
+zdbsp ${BASE_WAD_NAME}.wad -t -g          -o ${BASE_WAD_NAME}-zdbsp-gl-normal-uncompressed.wad
+zdbsp ${BASE_WAD_NAME}.wad -t -g    -X    -o ${BASE_WAD_NAME}-zdbsp-gl-extended-uncompressed.wad
+zdbsp ${BASE_WAD_NAME}.wad -t -g       -z -o ${BASE_WAD_NAME}-zdbsp-gl-normal-compressed.wad
+zdbsp ${BASE_WAD_NAME}.wad -t -g    -X -z -o ${BASE_WAD_NAME}-zdbsp-gl-extended-compressed.wad
 
-zdbsp freedoom2.wad -t -g -x       -o freedoom2-zdbsp-glonly-normal-uncompressed.wad
-zdbsp freedoom2.wad -t -g -x -X    -o freedoom2-zdbsp-glonly-extended-uncompressed.wad
-zdbsp freedoom2.wad -t -g -x    -z -o freedoom2-zdbsp-glonly-normal-compressed.wad
-zdbsp freedoom2.wad -t -g -x -X -z -o freedoom2-zdbsp-glonly-extended-compressed.wad
+zdbsp ${BASE_WAD_NAME}.wad -t -g -G       -o ${BASE_WAD_NAME}-zdbsp-glmatching-normal-uncompressed.wad
+zdbsp ${BASE_WAD_NAME}.wad -t -g -G -X    -o ${BASE_WAD_NAME}-zdbsp-glmatching-extended-uncompressed.wad
+zdbsp ${BASE_WAD_NAME}.wad -t -g -G    -z -o ${BASE_WAD_NAME}-zdbsp-glmatching-normal-compressed.wad
+zdbsp ${BASE_WAD_NAME}.wad -t -g -G -X -z -o ${BASE_WAD_NAME}-zdbsp-glmatching-extended-compressed.wad
 
-zdbsp freedoom2.wad -t -g --gl-v5  -o freedoom2-zdbsp-glv5-normal-uncompressed.wad
+zdbsp ${BASE_WAD_NAME}.wad -t -g -x       -o ${BASE_WAD_NAME}-zdbsp-glonly-normal-uncompressed.wad
+zdbsp ${BASE_WAD_NAME}.wad -t -g -x -X    -o ${BASE_WAD_NAME}-zdbsp-glonly-extended-uncompressed.wad
+zdbsp ${BASE_WAD_NAME}.wad -t -g -x    -z -o ${BASE_WAD_NAME}-zdbsp-glonly-normal-compressed.wad
+zdbsp ${BASE_WAD_NAME}.wad -t -g -x -X -z -o ${BASE_WAD_NAME}-zdbsp-glonly-extended-compressed.wad
 
-glbsp -q -v1 -xp freedoom2.wad -o freedoom2-glbsp-glv1.wad
-glbsp -q -v2 -xp freedoom2.wad -o freedoom2-glbsp-glv2.wad
-glbsp -q -v3 -xp freedoom2.wad -o freedoom2-glbsp-glv3.wad
-glbsp -q -v5 -xp freedoom2.wad -o freedoom2-glbsp-glv5.wad
+zdbsp ${BASE_WAD_NAME}.wad -t -g --gl-v5  -o ${BASE_WAD_NAME}-zdbsp-glv5-normal-uncompressed.wad
+
+glbsp -q -v1 -xp ${BASE_WAD_NAME}.wad -o ${BASE_WAD_NAME}-glbsp-glv1.wad
+glbsp -q -v2 -xp ${BASE_WAD_NAME}.wad -o ${BASE_WAD_NAME}-glbsp-glv2.wad
+glbsp -q -v3 -xp ${BASE_WAD_NAME}.wad -o ${BASE_WAD_NAME}-glbsp-glv3.wad
+glbsp -q -v5 -xp ${BASE_WAD_NAME}.wad -o ${BASE_WAD_NAME}-glbsp-glv5.wad
